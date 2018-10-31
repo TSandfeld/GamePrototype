7bbffbadb06df0a59d844bc8d5f69b5ac38bbdf9
TSandfeld <thomas.sandfeld@gmail.com>
2018-10-31 17:12:46 +0100

    TileMap start

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class CharacterController : MonoBehaviour {
+
+    [SerializeField]
+    float speed = 3.5f;
+
+    protected Vector2 direction;
+
+	// Use this for initialization
+	void Start () 
+    {
+		
+	}
+	
+	// Update is called once per frame
+    protected virtual void Update() 
+    {
+        Move();
+	}
+
+    public void Move()
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+}
